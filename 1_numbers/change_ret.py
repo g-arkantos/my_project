8979bfdf9fd8a175b6fbe860bf5acd0618bdf31a
g-arkantos <megrace.gn@gmail.com>
2021-01-08 22:05:01 +0530

    updates

--- 1_numbers/change_ret.py
+++ 1_numbers/change_ret.py
@@ -1,1 +1,27 @@
-Change Return Program - The user enters a cost and then the amount of money given. The program will figure out the change and the number of quarters, dimes, nickels, pennies needed for the change.
+'''Change Return Program - The user enters a cost and then the amount of money given. 
+The program will figure out the change and the number of quarters, dimes, nickels, pennies needed for the change.'''
+import math
+def main():
+	penny=0.01
+	nickel=penny*5
+	dime=penny*10
+	quarter=penny*25
+	dollar=int(penny*100)
+	print("dollar: ", dollar,'\n ', "nickel: ", nickel)
+	cost=11.14
+	get_change(cost)
+
+main()
+
+def get_change():
+	while True:
+		if cost>0:
+			dollar=math.floor(cost)
+			rem=cost-dollar
+			
+
+
+
+
+
+
